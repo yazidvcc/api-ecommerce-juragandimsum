1a651fc21540b2fb1b4ef59d37850582322ef734
Yazid Khairul Rizal <yazidrizal929@gmail.com>
2026-08-17 12:00:26 +0700

    update create order

--- src/validation/order-validation.js
+++ src/validation/order-validation.js
@@ -4,18 +4,7 @@ const createOrderValidation = Joi.object({
     province: Joi.string().required(),
     city: Joi.string().required(),
     district: Joi.string().required(),
-    postal_code: Joi.number().optional(),
-    spesifict_address: Joi.string().required(),
-    product: Joi.array().items(Joi.object({
-        product_id: Joi.number().required(),
-        quantity: Joi.number().min(1).positive().required()
-    })).required()
-});
-
-const createShippingCostOrderValidation = Joi.object({
-    order_id: Joi.number().positive().required(),
-    shipping_cost: Joi.number().positive().required(),
-    shipping_name: Joi.string().required()
+    sub_district: Joi.string().required()
 });
 
 const idOrderValidation = Joi.number().positive().required();
@@ -46,7 +35,6 @@ const statistictTime = Joi.object({
 
 export {
     createOrderValidation,
-    createShippingCostOrderValidation,
     idOrderValidation,
     searchOrderValidation,
     updateStatusOrderValidation,
